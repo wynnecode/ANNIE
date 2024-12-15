712bb6c8a93c13a03e77d3f3023aa6b419dd6a3a
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-12-15 21:28:21 +0530

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -1,9 +1,9 @@
 #
-# Copyright (C) 2024 by THE-VIP-BOY-OP@Github, < https://github.com/THE-VIP-BOY-OP >.
+# Copyright (C) 2024 by Moonshining6@Github, < https://github.com/THE-VIP-BOY-OP >.
 #
-# This file is part of < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC > project,
+# This file is part of < https://github.com/Moonshining6/ANNIE-MUSIC > project,
 # and is released under the MIT License.
-# Please see < https://github.com/THE-VIP-BOY-OP/VIP-MUSIC/blob/master/LICENSE >
+# Please see < https://github.com/Moonshining6/ANNIE-MUSIC/blob/master/LICENSE >
 #
 # All rights reserved
 
@@ -20,41 +20,56 @@ languages_present = {}
 
 
 def get_command(value: str) -> List:
-    return commands["command"][value]
+    return commands["command"].get(value, [])
 
 
 def get_string(lang: str):
-    return languages[lang]
+    return languages.get(lang, languages.get("en", {}))
 
 
+# Load command files
 for filename in os.listdir(r"./strings"):
     if filename.endswith(".yml"):
         language_name = filename[:-4]
-        commands[language_name] = yaml.safe_load(
-            open(r"./strings/" + filename, encoding="utf8")
-        )
+        try:
+            with open(r"./strings/" + filename, encoding="utf8") as file:
+                commands[language_name] = yaml.safe_load(file) or {}
+        except Exception as e:
+            print(f"Error loading command file '{filename}': {e}")
+            sys.exit()
 
-
+# Load language files
 for filename in os.listdir(r"./strings/langs/"):
     if "en" not in languages:
-        languages["en"] = yaml.safe_load(
-            open(r"./strings/langs/en.yml", encoding="utf8")
-        )
-        languages_present["en"] = languages["en"]["name"]
+        try:
+            with open(r"./strings/langs/en.yml", encoding="utf8") as file:
+                languages["en"] = yaml.safe_load(file) or {}
+                languages_present["en"] = languages["en"].get("name", "English")
+        except Exception as e:
+            print(f"Error loading default language file 'en.yml': {e}")
+            sys.exit()
+
     if filename.endswith(".yml"):
         language_name = filename[:-4]
         if language_name == "en":
             continue
-        languages[language_name] = yaml.safe_load(
-            open(r"./strings/langs/" + filename, encoding="utf8")
-        )
-        for item in languages["en"]:
-            if item not in languages[language_name]:
-                languages[language_name][item] = languages["en"][item]
-    try:
-        languages_present[language_name] = languages[language_name]["name"]
-    except:
-        print(
-            "There is some issue with the language file inside bot. Please report it to the Kitty at @grandxmasti on Telegram"
-        )
-        sys.exit()
+        try:
+            with open(r"./strings/langs/" + filename, encoding="utf8") as file:
+                languages[language_name] = yaml.safe_load(file) or {}
+
+            # Fill missing keys from "en"
+            for item in languages["en"]:
+                if item not in languages[language_name]:
+                    languages[language_name][item] = languages["en"][item]
+
+            languages_present[language_name] = languages[language_name].get(
+                "name", f"Unknown ({language_name})"
+            )
+        except Exception as e:
+            print(f"Error loading language file '{filename}': {e}")
+            sys.exit()
+
+# Final validation
+if "en" not in languages:
+    print("Error: Default 'en' language file is missing or invalid.")
+    sys.exit()
